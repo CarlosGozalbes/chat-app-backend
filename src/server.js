3ce2a1c9440cb8e41c8d6a84083a78b4de8ce01e
Carlos Gozalbes <carlosfgmendoza@gmail.com>
2023-01-10 17:46:45 +0100

    jkk

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import cors from "cors";
+import express from "express";
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+app.use(cors());
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
+
